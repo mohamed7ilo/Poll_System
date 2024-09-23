0836ffaf70e3e9bf0458f944f40c03c9cb611933
mohamed7ilo <mohamedhiloyt@gmail.com>
2024-09-23 03:18:43 +0300

    init

---
+++ PollSystem/src/main/java/com/PollSystem/PollSystem/PollSystemApplication.java
@@ -0,0 +1,13 @@
+package com.PollSystem.PollSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollSystemApplication.class, args);
+	}
+
+}
